0a34e6495c12f7fa750ab96bf0d93c154f7b60f7
dressalima <42049173+dressalima@users.noreply.github.com>
2018-09-17 19:33:15 -0300

    Create jogovelha.py

---
+++ src/jogovelha.py
@@ -0,0 +1,15 @@
+def  initializar ():
+	tab = []
+	para eu na  faixa ( 3 ):
+		linha = []
+		para j na  faixa ( 3 ):
+			linha.append ( " . " )
+		tab.append (linha)
+	guia de retorno
+
+def  main ():
+	jogo = inicializar ()
+	imprimir (jogo)
+
+if  __name__  ==  " __main__ " :
+	a Principal()
