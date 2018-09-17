1610b9a193d082eee9d485500834b8791d60dbdb
dressalima <42049173+dressalima@users.noreply.github.com>
2018-09-17 19:47:03 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -1,20 +1,20 @@
 import jogovelha
 import sys
 
-erroInicializar =  Falsa
-jogo = jogovelha.inicializar ()
+erroInicializar = False
+jogo = jogovelha.inicializar()
 
-se  len (jogo) ! =  3 :
-	erroInicializar =  Verdadeiro
-else :
-	para linha no jogo:
-		se  len (linha) ! =  3 :
-			erroInicializar =  Verdadeiro
-		else :
-			para o elemento na linha:
-				if elemento ! =  ' . ' :
-					erroInicializar =  Verdadeiro
-se erroInicializar:
-	sys.exit ( 1 )
-else :
-	sys.exit ( 0 )
+if len(jogo) != 3:
+	erroInicializar = True
+else:
+	for linha in jogo:
+		if len(linha) != 3:
+			erroInicializar = True
+		else:
+			for elemento in linha:
+				if elemento != '.':
+					erroInicializar = True
+if erroInicializar:
+	sys.exit(1)
+else:
+	sys.exit(0)
