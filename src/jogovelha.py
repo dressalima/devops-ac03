6a4a6944c54ab216357328503c9be5bf55adff46
dressalima <42049173+dressalima@users.noreply.github.com>
2018-09-17 19:48:01 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,15 +1,15 @@
-def  initializar ():
-	tab = []
-	para eu na  faixa ( 3 ):
-		linha = []
-		para j na  faixa ( 3 ):
-			linha.append ( " . " )
-		tab.append (linha)
-	guia de retorno
+def inicializar():
+	tab = [ ]
+	for i in range(3):
+		linha = [ ]
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
 
-def  main ():
-	jogo = inicializar ()
-	imprimir (jogo)
+def main( ):
+	jogo = inicializar( )
+	print (jogo)
 
-if  __name__  ==  " __main__ " :
-	a Principal()
+if __name__ == "__main__":
+	main()
